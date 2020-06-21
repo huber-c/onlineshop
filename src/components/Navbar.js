9c6c649995291644515a03b1ed23c719f4211756
Carina Huber <huber.carina2806@gmail.com>
2020-06-21 14:50:09 +0100

    ver_1.1:adjust text, small styling changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
     return(
             <nav className="nav-wrapper">
                 <div className="container">
-                    <Link to="/" className="brand-logo">Shopping</Link>
+                    <Link to="/" className="brand-logo">Shop Board Games</Link>
                     
                     <ul className="right">
                         <li><Link to="/">Shop</Link></li>
-                        <li><Link to="/cart">My cart</Link></li>
+                        <li><Link to="/cart">My items</Link></li>
                         <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
                     </ul>
                 </div>
